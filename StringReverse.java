7bfe85c78964501791ebd931a1571cf7760aedc5
Rudrarup365 <rudrarup.mitra@gmail.com>
2024-08-28 10:50:03 +0530

    Reverse a string with StringBuilder
    Reverse a string with mutable class StringBuilder

---
+++ StringReverse.java
@@ -0,0 +1,20 @@
+package javapackage;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String a="Terra firma";
+		System.out.println(reverse(a));
+	}
+public static String reverse(String in) {
+		if (in==null) {
+			throw new IllegalArgumentException ("Null is not validinput");
+		}
+		StringBuilder out =new StringBuilder();
+		char[] chars= in.toCharArray();
+		for (int i=chars.length-1; i>=0;i--) {
+			out.append(chars[i]);
+		}
+		return out.toString();
+}
+}
