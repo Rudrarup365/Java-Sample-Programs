d203de9a3bfc4368161ed6e33fe42ab375065a7f
Rudrarup Mitra <rudrarup.mitra@gmail.com>
2024-10-10 16:42:39 +0530

    Addition of 2 strings like integers

---
+++ StringAdd.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class StringAdd {
+
+	public static void main(String[] args) {
+		String a1="111000";
+		String a2="222";
+		int answer=Integer.parseInt(a1)+Integer.parseInt(a2);
+		System.out.println(answer);
+		
+	}
+	
+}
