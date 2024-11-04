69104a3af99cb6d8cc1821b0b5fbae6ba82bf8d1
Rudrarup Mitra <rudrarup.mitra@gmail.com>
2024-11-04 16:39:42 +0530

    Finding Greater and Smaller between 2 numbers
    Finding Greater and Smaller between 2 numbers without comparison operator

---
+++ GreaterAndSmallerOfTwoWithoutComparison.java
@@ -0,0 +1,18 @@
+package testing;
+
+public class GreaterAndSmallerOfTwoWithoutComparison {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = -30;
+		System.out.println("Greater of " + a + " and " + b + " is " + greater (a,b));
+		System.out.println("Smaller of " + a + " and " + b + " is " + smaller (a,b));
+	}
+	static int greater(int a, int b) {
+		return ((Math.abs(a-b)+(a+b))/2);
+	}
+	static int smaller(int a, int b) {
+		return (int) ((a+b-Math.sqrt((a-b)*(a-b)))/2);
+	}
+
+}
