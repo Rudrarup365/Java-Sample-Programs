3126096610c0a7f02fd4b9a72f89d09ec6f5652d
Rudrarup Mitra <rudrarup.mitra@gmail.com>
2024-11-04 16:43:55 +0530

    Add files via upload

--- GreaterAndSmallerOfTwoWithoutComparison.java
+++ GreaterAndSmallerOfTwoWithoutComparison.java
@@ -3,8 +3,8 @@ package testing;
 public class GreaterAndSmallerOfTwoWithoutComparison {
 
 	public static void main(String[] args) {
-		int a = 20;
-		int b = -30;
+		int a = -20;
+		int b = 30;
 		System.out.println("Greater of " + a + " and " + b + " is " + greater (a,b));
 		System.out.println("Smaller of " + a + " and " + b + " is " + smaller (a,b));
 	}
@@ -12,7 +12,7 @@ public class GreaterAndSmallerOfTwoWithoutComparison {
 		return ((Math.abs(a-b)+(a+b))/2);
 	}
 	static int smaller(int a, int b) {
-		return (int) ((a+b-(a-b))/2);
+		return (int) ((a+b-Math.sqrt((a-b)*(a-b)))/2);
 	}
 
 }
