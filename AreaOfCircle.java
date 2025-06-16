a233a5ea320395151070429dafc0165448a3eb30
Rudrarup Mitra <rudrarup.mitra@gmail.com>
2025-06-16 18:41:41 +0530

    Area of circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int radius=5;
+		double area;
+		area= radius * radius * Math.PI;
+		System.out.println("Area of the circle with radius = "+ radius + " is " + area);
+	}
+
+}
